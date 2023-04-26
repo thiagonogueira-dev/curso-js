82aade3fc4c8f80a4ec65e9320fb39c70da460bf
Thiago <th.nogueira21@gmail.com>
2023-04-26 00:15:42 -0300

    Html pt1

---
+++ Array/concat.js
@@ -0,0 +1,11 @@
+const filhas = ['Ualeskah', 'Cibalena'];
+const filhos = ['Uoxiton', 'Uesclei'];
+const todos = filhas.concat(filhos, 'Fulano');
+console.log(todos);
+
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]))
+
+const obj = {
+    nome: 'Th',
+    idade: 21
+}
---
+++ Array/flatMap.js
@@ -0,0 +1,37 @@
+const escola = [{
+    nome: 'Turma M1',
+    alunos: [{
+        nome: 'Gustavo',
+        nota: 8.1
+    }, {
+        nome: 'Ana',
+        nota: 9.3
+    }]
+}, {
+    nome: 'Turma M2',
+    alunos: [{
+        nome: 'Rebeca',
+        nota: 8.9
+    }, {
+        nome: 'Roberto',
+        nota: 7.3
+    }]
+}]
+
+const getNotaAluno = a => a.nota;
+const getNotasTurma = turma => turma.alunos.map(getNotaAluno);
+
+const notas1 = escola.map(getNotasTurma);
+console.log(notas1);
+
+Array.prototype.flatMap2 = function(callback){
+    return Array.prototype.concat.apply([], this.map(callback))
+}
+
+const notas2 = escola.flatMap2(getNotasTurma);
+console.log(notas2);
+
+const notas3 = escola.flatMap(getNotasTurma);
+console.log(notas3)
+
+// flatMap já existe!!
