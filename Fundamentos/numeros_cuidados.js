698156216222e5ceefe837de8d9df276c22d93bb
Thiago <th.nogueira21@gmail.com>
2023-04-30 01:29:39 -0300

    Html parte2

--- Fundamentos/numeros_cuidados.js
+++ Fundamentos/numeros_cuidados.js
@@ -1,7 +1,7 @@
 console.log(7 / 0)
 console.log("10" / 2)
 console.log("Show" / 2)
-console.log(0.1 + 0.7) // Imprecisão
+console.log((0.1 + 0.7).toFixed(2)) // Imprecisão
 // console.log(10.toString())
 console.log((10).toString())
 
