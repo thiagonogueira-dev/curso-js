9d928ebb1c52ab7550c25b4e84e4242e0d3e131c
Thiago <th.nogueira21@gmail.com>
2023-05-28 23:40:43 -0300

    Gulp

---
+++ Gulp/javascript/gulpfile.js
@@ -0,0 +1,26 @@
+const {series} = require('gulp');
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+
+function padrao(cb){
+    return gulp.src('./src/**/*.js')
+        .pipe(babel({
+            comments: false,
+            presets: ['env'],
+
+        }))
+        .pipe(uglify())
+        .on('error', err => console.log(err))
+        .pipe(concat('codigo.min.js'))
+        .pipe(gulp.dest('./build/js'));
+}
+
+function fim(cb){
+    console.log('fim!!');
+
+    return cb();
+}
+
+module.exports.default = series(padrao, fim);
