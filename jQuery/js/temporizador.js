28147a59218c6dafcdc55744e467196453056336
Thiago <th.nogueira21@gmail.com>
2023-06-29 23:11:08 -0300

    bootstrap

--- jQuery/js/temporizador.js
+++ jQuery/js/temporizador.js
@@ -5,12 +5,12 @@
             horario: '23:59:59'
         }, opcoes);
 
-        const horaDezena = $('<span class="digito">').html('0');
-        const horaUnidade = $('<span class="digito">').html('0');
-        const minutoDezena = $('<span class="digito">').html('0');
-        const minutoUnidade = $('<span class="digito">').html('0');
-        const segundoDezena = $('<span class="digito">').html('0');
-        const segundoUnidade = $('<span class="digito">').html('0');
+        const horaDezena = $('<span class="digito">');
+        const horaUnidade = $('<span class="digito">');
+        const minutoDezena = $('<span class="digito">');
+        const minutoUnidade = $('<span class="digito">');
+        const segundoDezena = $('<span class="digito">');
+        const segundoUnidade = $('<span class="digito">');
         
         const separadorHora = $('<span class="separador">').html(':');
         const separadorMinuto = $('<span class="separador">').html(':');
