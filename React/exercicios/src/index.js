5fb68a4e86b01c7d7028ec72ebe8bbdfcd1b5d66
Thiago <th.nogueira21@gmail.com>
2023-07-03 00:00:13 -0300

    React

---
+++ React/exercicios/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Primeiro from './componentes/primeiro';
+
+// const elemento = <h1>React 2</h1>
+
+ReactDOM.render(<Primeiro/>, document.getElementById('root'));
