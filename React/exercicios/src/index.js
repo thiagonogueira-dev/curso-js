529881abaea0cc7db622cd7a19c97ea9d8b17f61
Thiago <th.nogueira21@gmail.com>
2023-07-03 20:59:36 -0300

    React

--- React/exercicios/src/index.js
+++ React/exercicios/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
-import Primeiro from './componentes/primeiro';
+import Pai from './componentes/Pai';
+import Filho from './componentes/Filho';
 
-// const elemento = <h1>React 2</h1>
+const root = createRoot(document.getElementById('root'));
 
-ReactDOM.render(<Primeiro/>, document.getElementById('root'));
+root.render(
+    <div>
+        <Pai nome="Paulo" sobrenome="Silva">
+            <Filho nome="Thiago"/>
+            <Filho nome="Roger"/>
+            <Filho nome="Blau"/>
+            <Filho nome="Blue"/>
+        </Pai>
+    </div>
+);
